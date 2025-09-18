9b590aac728e0cfc4cc9fafd407da929bad50114
aya13hahaha <i.am.ayaka0413@gmail.com>
2025-09-18 17:02:10 +0900

    kadai

---
+++ htdocs/php-basic/variable.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+   <meta charset="UTF-8">
+   <title>PHP基礎編</title>
+</head>
+
+<body>
+   <p>
+       <?php
+       // 変数に値を代入する
+       $user_name = '侍太郎';
+
+       // 変数の中身を出力する
+       echo $user_name;
+
+       echo '<br>';
+
+       //　変数に値を再代入する
+       $user_name = '侍花子';
+
+       // 変数の中身を出力する
+       echo $user_name;
+       ?>
+   </p>
+</body>
+
+</html>
